20d60654167025819d2ad3836bdd82d88c277c4d
Annnbian <13941490028@163.com>
2023-08-14 20:54:15 +1200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button as VanButton } from 'vant'
+const store = useUserStore()
 </script>
 
 <template>
---
+++ src/stores/user.ts
@@ -0,0 +1,27 @@
+import type { User } from '@/types/user'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useUserStore = defineStore(
+  'cp-user',
+  () => {
+    // 1. state
+    const user = ref<User>()
+    // 2. set up user
+    const setUser = (u: User) => {
+      user.value = u
+    }
+    // 3. delete user
+    const delUser = () => {
+      user.value = undefined
+    }
+
+    const returnUrl = ref('')
+    const setReturnUrl = (url: string) => (returnUrl.value = url)
+
+    return { user, setUser, delUser, returnUrl, setReturnUrl }
+  },
+  {
+    persist: true
+  }
+)
