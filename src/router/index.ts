9f6e324db39ad490fed48d5be52955c58c255e8a
Annnbian <13941490028@163.com>
2023-08-15 19:55:24 +1200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'./login',
+      path:'/login',
       component: ()=> import('@/views/Login/index.vue')
     }
   ]
