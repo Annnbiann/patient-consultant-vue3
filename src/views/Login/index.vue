659b387cbb8b28047ef81ca7a41e0f509de67aaf
Annnbian <13941490028@163.com>
2023-08-19 11:15:22 +1200

    1

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,130 +1,102 @@
-<script setup lang="ts">
-// import { useMobileCode } from '@/composables'
-// import { loginByMobile, loginByPassword } from '@/services/user'
-// import { useUserStore } from '@/stores'
-// import { codeRules, mobileRules, passwordRules } from '@/utils/rules'
-import { showSuccessToast, showToast } from 'vant'
-import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-
-const mobile = ref('')
-const password = ref('')
-const agree = ref(false)
-
-const store = useUserStore()
-const router = useRouter()
-const route = useRoute()
-const onSubmit = async () => {
-  if (!agree.value) return showToast('Please tick')
-  // 进行登录(合并短信登录)
-  const res = isPass.value
-    ? await loginByPassword(mobile.value, password.value)
-    : await loginByMobile(mobile.value, code.value)
-  store.setUser(res.data)
-  showSuccessToast('Log in succuessfully!')
-  router.replace((route.query.returnUrl as string) || '/user')
-}
-
-// 短信登录界面切换
-const isPass = ref(true)
-const code = ref('')
-
-// 发送短信验证码
-const { onSend, time, form } = useMobileCode(mobile)
-
-// 密码的可见与不可见
-const isShow = ref(false)
-
-// http%3A%2F%2Fconsult-patients.itheima.net%2Flogin%2Fcallback
-
-const url =
-  'https://graph.qq.com/oauth2.0/authorize?client_id=102015968&response_type=token&scope=all&redirect_uri=' +
-  encodeURIComponent(import.meta.env.VITE_APP_CALLBACK + '/login/callback')
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="login-page">
     <cp-nav-bar
-      right-text="Register"
+      right-text="register"
       @click-right="$router.push('/register')"
     ></cp-nav-bar>
     <!-- 头部 -->
     <div class="login-head">
-      <h3>{{ isPass ? 'Password' : 'Code' }}</h3>
+      <h3>Password</h3>
       <a href="javascript:;">
-        <span @click="isPass = !isPass">
-          {{ isPass ? 'Text Code' : 'Password' }}
-        </span>
+        <span>Text code login</span>
         <van-icon name="arrow"></van-icon>
       </a>
     </div>
     <!-- 表单 -->
-    <van-form autocomplete="off" @submit="onSubmit" ref="form">
-      <van-field
-        name="mobile"
-        v-model="mobile"
-        :rules="mobileRules"
-        placeholder="Phone number"
-        type="tel"
-      ></van-field>
-      <van-field
-        v-if="isPass"
-        v-model="password"
-        :rules="passwordRules"
-        placeholder="Password"
-        :type="isShow ? 'text' : 'password'"
-      >
-        <template #button>
-          <cp-icon
-            :name="`login-eye-${isShow ? 'on' : 'off'}`"
-            @click="isShow = !isShow"
-            style="margin-right: 10px"
-          ></cp-icon>
-        </template>
-      </van-field>
-      <van-field
-        v-else
-        :rules="codeRules"
-        placeholder="Text code"
-        v-model="code"
-      >
-        <template #button>
-          <span class="btn-send" :class="{ active: time > 0 }" @click="onSend">
-            {{ time > 0 ? `${time}s later` : 'send code' }}
-          </span>
-        </template>
-      </van-field>
+    <van-form autocomplete="off">
+      <van-field placeholder="请输入手机号" type="tel"></van-field>
+      <van-field placeholder="请输入密码" type="password"></van-field>
       <div class="cp-cell">
-        <van-checkbox v-model="agree">
-          <span>I agree</span>
+        <van-checkbox>
+          <span>I agree on</span>
           <a href="javascript:;">user agreement</a>
           <span>and</span>
           <a href="javascript:;">privacy policy</a>
         </van-checkbox>
       </div>
       <div class="cp-cell">
-        <van-button native-type="submit" block round type="primary">
-          Log in
-        </van-button>
+        <van-button block round type="primary">log in</van-button>
       </div>
       <div class="cp-cell">
-        <a href="javascript:;">Forgot password</a>
+        <a href="javascript:;">forgot password？</a>
       </div>
     </van-form>
     <!-- 底部 -->
-    <div class="login-other">
-      <van-divider>Third party log in</van-divider>
-      <a
-        @click="store.setReturnUrl(route.query.returnUrl as string)"
-        class="icon"
-        :href="url"
-      >
+    <!-- <div class="login-other">
+      <van-divider>第三方登录</van-divider>
+      <div class="icon">
         <img src="@/assets/qq.svg" alt="" />
-      </a>
-    </div>
+      </div>
+    </div> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
-@import '@/styles/login.scss';
+.login {
+  &-page {
+    padding-top: 46px;
+  }
+  &-head {
+    display: flex;
+    padding: 30px 30px 50px;
+    justify-content: space-between;
+    align-items: flex-end;
+    line-height: 1;
+    h3 {
+      font-weight: normal;
+      font-size: 24px;
+    }
+    a {
+      font-size: 15px;
+    }
+  }
+  &-other {
+    margin-top: 60px;
+    padding: 0 30px;
+    .icon {
+      display: flex;
+      justify-content: center;
+      img {
+        width: 36px;
+        height: 36px;
+        padding: 4px;
+      }
+    }
+  }
+}
+.van-form {
+  padding: 0 14px;
+  .cp-cell {
+    height: 52px;
+    line-height: 24px;
+    padding: 14px 16px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    .van-checkbox {
+      a {
+        color: var(--cp-primary);
+        padding: 0 5px;
+      }
+    }
+  }
+  .btn-send {
+    color: var(--cp-primary);
+    &.active {
+      color: rgba(22,194,163,0.5);
+    }
+  }
+}
 </style>
