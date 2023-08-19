242a2ac471041fbf6f400b8b47875b8f7df9d921
Annnbian <13941490028@163.com>
2023-08-19 14:50:14 +1200

    1

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,33 @@ const router = createRouter({
     {
       path:'/login',
       component: ()=> import('@/views/Login/index.vue')
+    },
+    {
+      path:'/',
+      redirect: '/home',
+      component: ()=> import('@/views/Layout/index.vue'),
+      children: [
+        {
+          path: '/home',
+          component: () => import('@/views/Home/index.vue'),
+          meta: { title: 'Homepage' }
+        },
+        {
+          path: '/article',
+          component: () => import('@/views/Article/index.vue'),
+          meta: { title: 'Article' }
+        },
+        {
+          path: '/notify',
+          component: () => import('@/views/Notify/index.vue'),
+          meta: { title: 'Notification' }
+        },
+        {
+          path: '/user',
+          component: () => import('@/views/User/index.vue'),
+          meta: { title: 'Profile' }
+        }
+      ]
     }
   ]
 })
