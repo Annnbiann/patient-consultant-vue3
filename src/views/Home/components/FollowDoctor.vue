c221f9ba62e3b1fb2441f0fb0cee01ab4fd19995
Annnbian <13941490028@163.com>
2023-08-23 19:45:34 +1200

    1

--- src/views/Home/components/FollowDoctor.vue
+++ src/views/Home/components/FollowDoctor.vue
@@ -1,45 +1,34 @@
 <script setup lang="ts">
 import DoctorCard from './DoctorCard.vue'
-// import { useWindowSize } from '@vueuse/core'
-// import type { DoctorList } from '@/types/consult'
+import { useWindowSize } from '@vueuse/core'
+import type { DoctorList } from '@/types/consult'
 import { ref, onMounted } from 'vue'
-// import { getDoctorPage } from '@/services/consult'
+import { getDoctorPage } from '@/services/consult'
 
-// const { width } = useWindowSize()
+const { width } = useWindowSize()
 
-// 组件初始化获取设备宽度，页面尺寸发生改变获取设备的宽度
-// import { ref, onMounted, onUnmounted } from 'vue'
-// const width = ref(0)
-// const setWidth = () => (width.value = window.innerWidth)
-// onMounted(() => {
-//   setWidth()
-//   window.addEventListener('resize', setWidth)
-// })
-// onUnmounted(() => {
-//   window.removeEventListener('resize', setWidth)
-// })
-// const list = ref<DoctorList>([])
-// const loadData = async () => {
-//   const res = await getDoctorPage({ current: 1, pageSize: 5 })
-//   list.value = res.data.rows
-// }
-// onMounted(() => loadData())
+const list = ref<DoctorList>([])
+const loadData = async () => {
+  const res = await getDoctorPage({ current: 1, pageSize: 5 })
+  list.value = res.data.rows
+}
+onMounted(() => loadData())
+
 </script>
 
 <template>
   <div class="follow-doctor">
     <div className="head">
-      <p>Recomend</p>
-      <a href="javascript:;"> more<i class="van-icon van-icon-arrow" /></a>
+      <p>Recommend</p>
+      <a href="javascript:;"> More<i class="van-icon van-icon-arrow" /></a>
     </div>
     <div class="body">
-      <!-- swipe 组件 -->
-      <van-swipe
-        :width="(150 / 375) * width"
+      <!-- swipe  -->
+      <van-swipe :width="(150/375) * width"
         :showIndicators="false"
         :loop="false"
       >
-        <van-swipe-item v-for="item in 5" :key="item.id">
+        <van-swipe-item v-for="item in list" :key="item.id">
           <doctor-card :item="item"></doctor-card>
         </van-swipe-item>
       </van-swipe>
