cf72c52bdcbf2e6fe997244e4d13fbf7c3ae4954
Annnbian <13941490028@163.com>
2023-09-14 22:21:46 +1200

    1

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -2,6 +2,59 @@
 import RoomStatus from './components/RoomStatus.vue'
 import RoomAction from './components/RoomAction.vue'
 import RoomMessage from './components/RoomMessage.vue'
+import io, { Socket } from 'socket.io-client'
+import { onMounted, onUnmounted } from 'vue'
+import { baseURL } from '@/utils/request'
+import { useUserStore } from '@/stores'
+import { useRoute } from 'vue-router'
+import type { Message, TimeMessages } from '@/types/room'
+import { MsgType, OrderType } from '@/enums'
+import { ref, nextTick, provide } from 'vue'
+import type { ConsultOrderItem, Image } from '@/types/consult'
+import { getConsultOrderDetail } from '@/services/consult'
+// import dayjs from 'dayjs'
+import { showToast } from 'vant'
+
+const store = useUserStore()
+const route = useRoute()
+
+let socket: Socket
+onUnmounted(() => {
+  socket.close()
+})
+onMounted(async () => {
+  // 建立链接，创建 socket.io 实例
+  socket = io(baseURL, {
+    auth: {
+      token: `Bearer ${store.user?.token}`
+    },
+    query: {
+      orderId: route.query.orderId
+    }
+  })
+
+  socket.on('connect', () => {
+    // 建立连接成功
+    console.log('connect')
+  })
+
+  socket.on('error', (event) => {
+    // 错误异常消息
+    console.log('error')
+  })
+
+  socket.on('disconnect', ()=> {
+    // 已经断开连接
+    console.log('disconnect')
+  })
+})
+
+
+
+
+
+
+
 </script>
 
 <template>
