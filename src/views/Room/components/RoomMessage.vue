ffc946df09ca1c6d8b6f0fd0d7e21342231d89f2
Annnbian <13941490028@163.com>
2023-09-30 22:26:23 +1300

    1

--- src/views/Room/components/RoomMessage.vue
+++ src/views/Room/components/RoomMessage.vue
@@ -1,48 +1,68 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+// import { useShowPrescription } from '@/composables'
+import { MsgType, PrescriptionStatus } from '@/enums'
+import { useUserStore } from '@/stores'
+import type { Image } from '@/types/consult'
+import type { Message, Prescription } from '@/types/room'
+// import { getConsultFlagText, getIllnessTimeText } from '@/utils/filter'
+// import dayjs from 'dayjs'
+// import { showImagePreview, showToast } from 'vant'
+// import EvaluateCard from './EvaluateCard.vue'
+import { useRouter } from 'vue-router'
+defineProps<{
+  item: Message
+}>()
+</script>
 
 <template>
   <!-- 患者卡片 -->
-  <div class="msg msg-illness">
+  <div class="msg msg-illness" v-if="item.msgType === MsgType.CardPat">
     <div class="patient van-hairline--bottom">
-      <p>李富贵 男 31岁</p>
-      <p>一周内 | 未去医院就诊</p>
+      <p>{{ item.msg.consultRecord?.patientInfo.name }}
+        {{ item.msg.consultRecord?.patientInfo.genderValue }}
+        {{ item.msg.consultRecord?.patientInfo.age }}yo</p>
+      <p v-if="item.msg.consultRecord">
+        {{ item.msg.consultRecord?.illnessTime }} |
+        {{ item.msg.consultRecord?.consultFlag }}
+      </p>
     </div>
     <van-row>
       <van-col span="6">病情描述</van-col>
-      <van-col span="18">头痛、头晕、恶心</van-col>
+      <van-col span="18">{{ item.msg.consultRecord?.illnessDesc }}</van-col>
       <van-col span="6">图片</van-col>
       <van-col span="18">点击查看</van-col>
     </van-row>
   </div>
   <!-- 通知-通用 -->
-  <div class="msg msg-tip">
+  <!-- <div class="msg msg-tip">
     <div class="content">
       <span>医护人员正在赶来，请耐心等候</span>
     </div>
-  </div>
+  </div> -->
   <!-- 通知-温馨提示 -->
-  <div class="msg msg-tip">
+  <!-- <div class="msg msg-tip">
     <div class="content">
       <span class="green">温馨提示：</span>
       <span>在线咨询不能代替面诊，医护人员建议仅供参考</span>
     </div>
-  </div>
+  </div> -->
   <!-- 通知-结束 -->
-  <div class="msg msg-tip msg-tip-cancel">
+  <!-- <div class="msg msg-tip msg-tip-cancel">
     <div class="content">
       <span>订单取消</span>
     </div>
-  </div>
+  </div> -->
   <!-- 发送文字 -->
-    <div class="msg msg-to">
+    <!-- <div class="msg msg-to">
     <div class="content">
       <div class="time">20:12</div>
       <div class="pao">大夫你好？</div>
     </div>
     <van-image src="https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/popular_3.jpg" />
-  </div>
+  </div> -->
   <!-- 发送图片 -->
-  <div class="msg msg-to">
+  <!-- <div class="msg msg-to">
     <div class="content">
       <div class="time">20:12</div>
       <van-image 
@@ -51,17 +71,17 @@
       />
     </div>
     <van-image src="https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/popular_3.jpg" />
-  </div>
+  </div> -->
   <!-- 接收文字 -->
-  <div class="msg msg-from">
+  <!-- <div class="msg msg-from">
     <van-image src="https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/popular_3.jpg" />
     <div class="content">
       <div class="time">20:12</div>
       <div class="pao">哪里不舒服</div>
     </div>
-  </div>
+  </div> -->
   <!-- 接收图片 -->
-  <div class="msg msg-from">
+  <!-- <div class="msg msg-from">
     <van-image src="https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/popular_3.jpg" />
     <div class="content">
       <div class="time">20:12</div>
@@ -70,9 +90,9 @@
         src="https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/popular_3.jpg"
       />
     </div>
-  </div>
+  </div> -->
   <!-- 处方卡片 -->
-  <div class="msg msg-recipe">
+  <!-- <div class="msg msg-recipe">
     <div class="content">
       <div class="head van-hairline--bottom">
         <div class="head-tit">
@@ -93,7 +113,7 @@
       </div>
       <div class="foot"><span>购买药品</span></div>
     </div>
-  </div>
+  </div> -->
   <!-- 评价卡片，后期实现 -->
 </template>
 
--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -15,13 +15,16 @@ import { getConsultOrderDetail } from '@/services/consult'
 // import dayjs from 'dayjs'
 import { showToast } from 'vant'
 
+
+
+
 const store = useUserStore()
 const route = useRoute()
 
+const list = ref<Message[]>([])
+const initialMsg = ref(true)
 let socket: Socket
-onUnmounted(() => {
-  socket.close()
-})
+
 onMounted(async () => {
   // 建立链接，创建 socket.io 实例
   socket = io(baseURL, {
@@ -47,9 +50,31 @@ onMounted(async () => {
     // 已经断开连接
     console.log('disconnect')
   })
+  
+ 
+    // 聊天记录
+    socket.on('chatMsgList', ({ data }: { data: TimeMessages[] }) => {
+    // 准备转换常规消息列表
+    const arr: Message[] = []
+    data.forEach((item, i) => {
+      arr.push({
+        msgType: MsgType.Notify,
+        msg: { content: item.createTime },
+        createTime: item.createTime,
+        id: item.createTime
+      })
+      arr.push(...item.items)
+    })
+    // 追加到聊天消息列表
+    list.value.unshift(...arr)
+  })
+
+
 })
 
-
+onUnmounted(() => {
+  socket.close()
+})
 
 
 
@@ -61,7 +86,10 @@ onMounted(async () => {
   <div class="room-page">
     <cp-nav-bar title="Consultation Room" />
     <room-status></room-status>
-    <room-message></room-message>
+    <room-message 
+        v-for="item in list"
+        :key="item.id"
+        :item="item"></room-message>
     <room-action></room-action>
   </div>
 </template>
